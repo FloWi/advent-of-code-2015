58679e2413823c7e4aced619916536fe7579baac
Florian Witteler <github@flwi.de>
2021-01-14 19:53:17 +0100

    solved day02

--- src/main/scala/day02/day02.scala
+++ src/main/scala/day02/day02.scala
@@ -10,7 +10,10 @@ object part1 {
       .filterNot(_.isEmpty())
       .toList
 
-    val solution = ???
+    val solution = input
+      .map(Day02.parse)
+      .map(Day02.calculateWrappingPaper)
+      .sum
 
     println(
       s"Solution for ${getCallingMainClass.getCanonicalName}: $solution"
@@ -27,7 +30,10 @@ object part2 {
       .filterNot(_.isEmpty())
       .toList
 
-    val solution = ???
+    val solution = input
+      .map(Day02.parse)
+      .map(Day02.calculateRibbon)
+      .sum
 
     println(
       s"Solution for ${getCallingMainClass.getCanonicalName}: $solution"
@@ -36,5 +42,37 @@ object part2 {
 }
 
 object Day02 {
+
+  case class Prism(l: Int, w: Int, h: Int)
+
+  def parse(line: String): Prism = {
+    val List(l, w, h) = line.split("x").toList.map(_.toInt)
+    Prism(l, w, h)
+  }
+
+  def calculateWrappingPaper(prism: Prism): Int = {
+    val Prism(l, w, h) = prism
+    val sides = List(
+      l * w,
+      w * h,
+      h * l
+    )
+    val smallestSide = sides.min
+    val total = sides.sum * 2 + smallestSide
+    total
+  }
+
+  def calculateRibbon(prism: Prism): Int = {
+    val Prism(l, w, h) = prism
+    val sidePerimeters = List(
+      2 * (l + w),
+      2 * (w + h),
+      2 * (h + l)
+    )
+    val smallestPerimeter = sidePerimeters.min
+    val volume = l * w * h
+    val total = smallestPerimeter + volume
+    total
+  }
 
 }
--- src/test/scala/day02/Day02Test.scala
+++ src/test/scala/day02/Day02Test.scala
@@ -3,11 +3,20 @@ package day02
 import day02._
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
+import day02.Day02.Prism
 
 class Day02Part1Test extends AnyFunSuite with Matchers {
 
   test("example 1") {
 
+    Day02.parse("2x3x4") shouldBe Prism(2, 3, 4)
+    Day02.calculateWrappingPaper(Day02.parse("2x3x4")) shouldBe 58
+  }
+
+  test("example 2") {
+
+    Day02.parse("1x1x10") shouldBe Prism(1, 1, 10)
+    Day02.calculateWrappingPaper(Day02.parse("1x1x10")) shouldBe 43
   }
 
 }
@@ -15,6 +24,11 @@ class Day02Part1Test extends AnyFunSuite with Matchers {
 class Day02Part2Test extends AnyFunSuite with Matchers {
 
   test("example 1") {
+    Day02.calculateRibbon(Day02.parse("2x3x4")) shouldBe 34
+
+  }
+  test("example 2") {
+    Day02.calculateRibbon(Day02.parse("1x1x10")) shouldBe 14
 
   }
 
