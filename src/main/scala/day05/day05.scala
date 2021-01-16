c96398f59e6811b040ec1fcd600fa3ecf278a70b
Florian Witteler <github@flwi.de>
2021-01-17 00:53:29 +0100

    solved day5 part1

--- src/main/scala/day05/day05.scala
+++ src/main/scala/day05/day05.scala
@@ -1,4 +1,5 @@
 package day05
+import day05.Day05._
 import helper.Helper._
 
 object part1 {
@@ -10,7 +11,7 @@ object part1 {
       .filterNot(_.isEmpty())
       .toList
 
-    val solution = ???
+    val solution = input.count(isNiceString)
 
     println(
       s"Solution for ${getCallingMainClass.getCanonicalName}: $solution"
@@ -36,5 +37,25 @@ object part2 {
 }
 
 object Day05 {
+
+  def isNiceString(line: String): Boolean = {
+    /*
+It contains at least three vowels (aeiou only), like aei, xazegov, or aeiouaeiouaeiou.
+It contains at least one letter that appears twice in a row, like xx, abcdde (dd), or aabbccdd (aa, bb, cc, or dd).
+It does not contain the strings ab, cd, pq, or xy, even if they are part of one of the other requirements.
+     */
+
+    val vowels = "aeiou".toCharArray.toSet
+    !line.contains("ab") &&
+    !line.contains("cd") &&
+    !line.contains("pq") &&
+    !line.contains("xy") &&
+    line.count(vowels.contains) >= 3 && {
+      line.toCharArray
+        .sliding(2)
+        .exists(chars => chars(0) == chars(1))
+    }
+
+  }
 
 }
--- src/test/scala/day05/Day05Test.scala
+++ src/test/scala/day05/Day05Test.scala
@@ -8,14 +8,25 @@ class Day05Part1Test extends AnyFunSuite with Matchers {
 
   test("example 1") {
 
+    /*
+    ugknbfddgicrmopn is nice because it has at least three vowels (u...i...o...), a double letter (...dd...), and none of the disallowed substrings.
+    aaa is nice because it has at least three vowels and a double letter, even though the letters used by different rules overlap.
+    jchzalrnumimnmhp is naughty because it has no double letter.
+    haegwjzuvuyypxyu is naughty because it contains the string xy.
+    dvszwmarrgswjxmb is naughty because it contains only one vowel.
+     */
+
+    Day05.isNiceString("ugknbfddgicrmopn") shouldBe true
+    Day05.isNiceString("aaa") shouldBe true
+    Day05.isNiceString("jchzalrnumimnmhp") shouldBe false
+    Day05.isNiceString("haegwjzuvuyypxyu") shouldBe false
+    Day05.isNiceString("dvszwmarrgswjxmb") shouldBe false
   }
 
 }
 
 class Day05Part2Test extends AnyFunSuite with Matchers {
 
-  test("example 1") {
-
-  }
+  test("example 1") {}
 
 }
