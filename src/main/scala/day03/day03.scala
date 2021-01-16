c7eb446ddf9ba28fbc03e53b60d21266af7532d4
Florian Witteler <github@flwi.de>
2021-01-16 23:19:28 +0100

    solved day3

--- src/main/scala/day03/day03.scala
+++ src/main/scala/day03/day03.scala
@@ -1,4 +1,6 @@
 package day03
+import cats.instances.char
+import day03.Day03.Pos
 import helper.Helper._
 
 object part1 {
@@ -10,12 +12,30 @@ object part1 {
       .filterNot(_.isEmpty())
       .toList
 
-    val solution = ???
+    val solution = solve(input.head)
 
     println(
       s"Solution for ${getCallingMainClass.getCanonicalName}: $solution"
     )
   }
+
+  def solve(line: String): Int = {
+    val (lastPos, visited) = line.toCharArray
+      .foldLeft((Pos(0, 0), Set(Pos(0, 0)))) { case ((currentPos, pastPositions), char) =>
+        val offset = char match {
+          case '^' => Pos(0, 1)
+          case 'v' => Pos(0, -1)
+          case '<' => Pos(-1, 0)
+          case '>' => Pos(1, 0)
+        }
+
+        val newPos = currentPos + offset
+        (newPos, pastPositions.+(newPos))
+      }
+
+    visited.size
+  }
+
 }
 
 object part2 {
@@ -27,14 +47,43 @@ object part2 {
       .filterNot(_.isEmpty())
       .toList
 
-    val solution = ???
+    val solution = solve(input.head)
 
     println(
       s"Solution for ${getCallingMainClass.getCanonicalName}: $solution"
     )
   }
+
+  def solve(line: String): Int = {
+    val (_, _, visited) = line.toCharArray.zipWithIndex
+      .foldLeft((Pos(0, 0), Pos(0, 0), Set(Pos(0, 0)))) { case ((santaPos, roboSantaPos, pastPositions), (char, index)) =>
+        val offset = char match {
+          case '^' => Pos(0, 1)
+          case 'v' => Pos(0, -1)
+          case '<' => Pos(-1, 0)
+          case '>' => Pos(1, 0)
+        }
+
+        val (newSantaPos, newRoboSantaPos) = if (index % 2 == 0) {
+          (santaPos + offset, roboSantaPos)
+
+        } else {
+          (santaPos, roboSantaPos + offset)
+        }
+
+        (newSantaPos, newRoboSantaPos, pastPositions.+(newSantaPos).+(newRoboSantaPos))
+      }
+
+    visited.size
+  }
+
 }
 
 object Day03 {
+  case class Pos(x: Int, y: Int) {
+    def +(other: Pos): Pos = {
+      Pos(x + other.x, y + other.y)
+    }
+  }
 
 }
--- src/test/scala/day03/Day03Test.scala
+++ src/test/scala/day03/Day03Test.scala
@@ -5,9 +5,18 @@ import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 
 class Day03Part1Test extends AnyFunSuite with Matchers {
-
+//> delivers presents to 2 houses: one at the starting location, and one to the east.
+  //^>v< delivers presents to 4 houses in a square, including twice to the house at his starting/ending location.
+  //^v^v^v^v^v delivers a bunch of presents to some very lucky children at only 2 houses.
   test("example 1") {
+    part1.solve(">") shouldBe 2
+  }
+  test("example 2") {
+    part1.solve("^>v<") shouldBe 4
 
+  }
+  test("example 3") {
+    part1.solve("^v^v^v^v^v") shouldBe 2
   }
 
 }
@@ -15,7 +24,14 @@ class Day03Part1Test extends AnyFunSuite with Matchers {
 class Day03Part2Test extends AnyFunSuite with Matchers {
 
   test("example 1") {
+    part2.solve("^v") shouldBe 3
+  }
+  test("example 2") {
+    part2.solve("^>v<") shouldBe 3
 
+  }
+  test("example 3") {
+    part2.solve("^v^v^v^v^v") shouldBe 11
   }
 
 }
