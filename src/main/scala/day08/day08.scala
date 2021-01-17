1985913cc47ead7a509676454cfa7113925c3b77
Florian Witteler <github@flwi.de>
2021-01-17 21:40:29 +0100

    solved day8 part1

--- src/main/scala/day08/day08.scala
+++ src/main/scala/day08/day08.scala
@@ -1,7 +1,53 @@
 package day08
+import cats.parse.{Parser1, Parser => P}
 import helper.Helper._
 
 object part1 {
+  def solve(input: List[String]): Int = {
+
+    val results = input.map(inp => (inp, parser.parse(inp)))
+
+    results.foreach(println)
+
+    val resultLengths = results.map {
+      case (_, Right((_, res))) =>
+        res.length
+      case (input, Left(error)) =>
+        println(s"\nparsing error. \nInput: $input")
+        println(s"Error: $error")
+
+        0
+
+    }
+
+    val inputLengths = input.map(_.length)
+
+    inputLengths.sum - resultLengths.sum
+  }
+
+  val parser: P[String] = {
+
+    val backslash: Parser1[String] = P.char('\\').string.backtrack
+    val hexDigit = P.charIn('a'.to('f') ++ ('0' to '9')).backtrack
+    val twoDigits = (hexDigit ~ hexDigit).map { case (c1, c2) =>
+      val intCode = Integer.parseInt(s"$c1$c2", 16)
+      new String(Character.toChars(intCode))
+    }.backtrack
+
+    val doubleQuote = P.string1(P.char('"')).backtrack
+
+    val hexCode: Parser1[String] = (P.char('x') *> twoDigits).backtrack
+    val escaped: Parser1[String] = (P.char('\\') *> P.oneOf(List(backslash, hexCode, doubleQuote))).backtrack
+
+    val word = P.rep1(P.charIn('a' to 'z'), 1).map(_.toList.mkString).backtrack
+
+    val parser: Parser1[String] = P.oneOf1(List(escaped, word)).backtrack
+
+    val p: P[String] = P.rep(parser).map(_.mkString).surroundedBy(P.char('"'))
+
+    p
+
+  }
 
   def main(args: Array[String]) = {
 
@@ -10,7 +56,7 @@ object part1 {
       .filterNot(_.isEmpty())
       .toList
 
-    val solution = ???
+    val solution = solve(input)
 
     println(
       s"Solution for ${getCallingMainClass.getCanonicalName}: $solution"
@@ -35,6 +81,4 @@ object part2 {
   }
 }
 
-object Day08 {
-
-}
+object Day08 {}
--- src/test/scala/day08/Day08Test.scala
+++ src/test/scala/day08/Day08Test.scala
@@ -1,12 +1,48 @@
 package day08
 
-import day08._
+import helper.Helper.source
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 
 class Day08Part1Test extends AnyFunSuite with Matchers {
 
+  val input = source(Some("src/test/resources/day08-example.txt"))
+    .getLines()
+    .filterNot(_.isEmpty())
+    .toList
+
+  test("example") {
+    part1.solve(input) shouldBe 23 - 11
+  }
+
   test("example 1") {
+    part1.solve(input.take(1)) shouldBe 2 - 0
+  }
+  test("example 2") {
+    part1.solve(input.slice(1, 2)) shouldBe 5 - 3
+  }
+
+  test("example 3") {
+    part1.solve(input.slice(2, 3)) shouldBe 10 - 7
+  }
+
+  test("example 4") {
+    part1.solve(input.slice(3, 4)) shouldBe 6 - 1
+  }
+
+  test("surrounded by") {
+    import cats.parse.{Parser => P}
+
+    val word = P.rep(P.charIn('a' to 'z')).map(_.mkString)
+    val p = word.surroundedBy(P.char('"'))
+    p.parse(""""aaa"""") shouldBe Right(("", "aaa"))
+  }
+
+  test("hexcode") {
+    val input = """"v\x27""""
+    val actual = part1.parser.parse(input)
+
+    actual shouldBe Right(("", "v'"))
 
   }
 
@@ -14,8 +50,6 @@ class Day08Part1Test extends AnyFunSuite with Matchers {
 
 class Day08Part2Test extends AnyFunSuite with Matchers {
 
-  test("example 1") {
-
-  }
+  test("example 1") {}
 
 }
