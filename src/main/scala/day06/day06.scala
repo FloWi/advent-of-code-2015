7f6b8d395b90b079926fa165c3f708aa67a4c79a
Florian Witteler <github@flwi.de>
2021-01-17 15:12:47 +0100

    solved day6

--- src/main/scala/day06/day06.scala
+++ src/main/scala/day06/day06.scala
@@ -1,5 +1,8 @@
 package day06
+import day06.Day06._
 import helper.Helper._
+
+import scala.reflect.ClassTag
 
 object part1 {
 
@@ -10,11 +13,35 @@ object part1 {
       .filterNot(_.isEmpty())
       .toList
 
-    val solution = ???
+    val grid = createGrid(width = 1000, height = 1000, default = false)
+    val instructions = input.map(parseInstruction)
+
+    instructions.foreach(applyInstruction(grid, _))
+    val solution = grid.map(line => line.count(identity)).sum
 
     println(
       s"Solution for ${getCallingMainClass.getCanonicalName}: $solution"
     )
+  }
+
+  def applyInstruction(grid: Array[Array[Boolean]], instruction: Instruction): Unit = {
+    instruction match {
+      case TurnOn(start, endInclusive) =>
+        for {
+          x <- start.x.to(endInclusive.x)
+          y <- start.y.to(endInclusive.y)
+        } grid(y)(x) = true
+      case TurnOff(start, endInclusive) =>
+        for {
+          x <- start.x.to(endInclusive.x)
+          y <- start.y.to(endInclusive.y)
+        } grid(y)(x) = false
+      case Toggle(start, endInclusive) =>
+        for {
+          x <- start.x.to(endInclusive.x)
+          y <- start.y.to(endInclusive.y)
+        } grid(y)(x) = !grid(y)(x)
+    }
   }
 }
 
@@ -27,14 +54,77 @@ object part2 {
       .filterNot(_.isEmpty())
       .toList
 
-    val solution = ???
+    val grid = createGrid(width = 1000, height = 1000, default = 0)
+
+    val instructions = input.map(parseInstruction)
+
+    instructions.foreach(applyInstruction(grid, _))
+    val solution = grid.map(line => line.sum).sum
 
     println(
       s"Solution for ${getCallingMainClass.getCanonicalName}: $solution"
     )
   }
+
+  def applyInstruction(grid: Array[Array[Int]], instruction: Instruction): Unit = {
+    val (lightnessIncreate, start, end) = instruction match {
+      case TurnOn(start, endInclusive) =>
+        (1, start, endInclusive)
+      case TurnOff(start, endInclusive) =>
+        (-1, start, endInclusive)
+      case Toggle(start, endInclusive) =>
+        (2, start, endInclusive)
+    }
+
+    for {
+      x <- start.x.to(end.x)
+      y <- start.y.to(end.y)
+    } {
+      grid(y)(x) += lightnessIncreate
+      if (grid(y)(x) < 0) grid(y)(x) = 0
+    }
+  }
+
 }
 
 object Day06 {
 
+  def createGrid[T: ClassTag](width: Int, height: Int, default: T): Array[Array[T]] = {
+    Array.fill(height)(Array.fill(width)(default))
+  }
+
+  sealed trait Instruction
+
+  case class Pos(x: Int, y: Int)
+
+  case class TurnOn(start: Pos, endInclusive: Pos) extends Instruction
+  case class TurnOff(start: Pos, endInclusive: Pos) extends Instruction
+  case class Toggle(start: Pos, endInclusive: Pos) extends Instruction
+
+  def parseCoords(str: String): (Pos, Pos) = {
+    val List(start, end) = str.split(" through ").toList.map(parseCoord)
+    (start, end)
+  }
+
+  def parseCoord(str: String): Pos = {
+    val List(x, y) = str.split(",").toList
+    Pos(x.toInt, y.toInt)
+  }
+
+  def parseInstruction(str: String): Instruction = {
+    if (str.startsWith("turn on ")) {
+      val (start, end) = parseCoords(str.replace("turn on ", ""))
+
+      TurnOn(start, end)
+
+    } else if (str.startsWith("turn off ")) {
+      val (start, end) = parseCoords(str.replace("turn off ", ""))
+      TurnOff(start, end)
+
+    } else if (str.startsWith("toggle ")) {
+      val (start, end) = parseCoords(str.replace("toggle ", ""))
+      Toggle(start, end)
+
+    } else ???
+  }
 }
--- src/test/scala/day06/Day06Test.scala
+++ src/test/scala/day06/Day06Test.scala
@@ -1,21 +1,49 @@
 package day06
 
-import day06._
+import day06.Day06._
+import day06.part1._
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 
 class Day06Part1Test extends AnyFunSuite with Matchers {
+  /*
+    turn on 0,0 through 999,999 would turn on (or leave on) every light.
+    toggle 0,0 through 999,0 would toggle the first line of 1000 lights, turning off the ones that were on, and turning on the ones that were off.
+    turn off 499,499 through 500,500 would turn off (or leave off) the middle four lights.
+   */
 
-  test("example 1") {
+  test("example 1") {}
 
+  test("parse instruction") {
+    Day06.parseInstruction("turn on 0,0 through 2,2") shouldBe TurnOn(Pos(0, 0), Pos(2, 2))
+    Day06.parseInstruction("turn off 0,0 through 2,2") shouldBe TurnOff(Pos(0, 0), Pos(2, 2))
+    Day06.parseInstruction("toggle 0,0 through 2,2") shouldBe Toggle(Pos(0, 0), Pos(2, 2))
+  }
+
+  test("turn on") {
+    val grid = createGrid(3, 3, false)
+    applyInstruction(grid, TurnOn(Pos(0, 0), Pos(0, 0)))
+    grid.flatten.count(identity) shouldBe 1
+  }
+
+  test("toggle") {
+    val grid = createGrid(3, 3, false)
+    applyInstruction(grid, Toggle(Pos(0, 0), Pos(0, 0)))
+    grid.flatten.count(identity) shouldBe 1
+    applyInstruction(grid, Toggle(Pos(0, 0), Pos(0, 0)))
+    grid.flatten.count(identity) shouldBe 0
+  }
+  test("have you tried turning it on and off again") {
+    val grid = createGrid(3, 3, false)
+    applyInstruction(grid, TurnOn(Pos(0, 0), Pos(0, 0)))
+    applyInstruction(grid, TurnOff(Pos(0, 0), Pos(0, 0)))
+    grid.flatten.count(identity) shouldBe 0
   }
 
 }
 
 class Day06Part2Test extends AnyFunSuite with Matchers {
 
-  test("example 1") {
-
-  }
+  test("example 1") {}
 
 }
