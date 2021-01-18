99f4e3b1bc3cc8cfe1afd42d5e2468771e0ce87f
Florian Witteler <github@flwi.de>
2021-01-18 17:03:09 +0100

    solved day13

--- src/main/scala/day13/day13.scala
+++ src/main/scala/day13/day13.scala
@@ -1,7 +1,9 @@
 package day13
+import com.typesafe.scalalogging.LazyLogging
+import day13.Day13.solve
 import helper.Helper._
 
-object part1 {
+object part1 extends LazyLogging {
 
   def main(args: Array[String]) = {
 
@@ -10,15 +12,25 @@ object part1 {
       .filterNot(_.isEmpty())
       .toList
 
-    val solution = ???
+    val solution = solve(calculateHappinessMap(input))
 
     println(
       s"Solution for ${getCallingMainClass.getCanonicalName}: $solution"
     )
   }
+
+  def calculateHappinessMap(input: List[String]): Map[String, Map[String, Int]] = {
+    val happinessScores = input.map(Day13.parse)
+    val initialMap = happinessScores.map(_._1).distinct.map(name => name -> Map.empty[String, Int]).toMap
+
+    happinessScores.foldLeft(initialMap) { case (acc, (p1, (p2, score))) =>
+      val map = acc(p1)
+      acc.updated(p1, map.updated(p2, score))
+    }
+  }
 }
 
-object part2 {
+object part2 extends LazyLogging {
 
   def main(args: Array[String]) = {
 
@@ -27,14 +39,74 @@ object part2 {
       .filterNot(_.isEmpty())
       .toList
 
-    val solution = ???
+    val solution = solve(calculateHappinessMap(input))
 
     println(
       s"Solution for ${getCallingMainClass.getCanonicalName}: $solution"
     )
   }
+
+  def calculateHappinessMap(input: List[String]): Map[String, Map[String, Int]] = {
+    val happinessScores = input.map(Day13.parse)
+    val people = happinessScores.map(_._1)
+    val updatedHappinessScores = happinessScores ::: people.flatMap { p =>
+      List(
+        ("me", (p, 0)),
+        (p, ("me", 0))
+      )
+    }
+
+    val initialMap = updatedHappinessScores.map(_._1).distinct.map(name => name -> Map.empty[String, Int]).toMap
+
+    updatedHappinessScores.foldLeft(initialMap) { case (acc, (p1, (p2, score))) =>
+      val map = acc(p1)
+      acc.updated(p1, map.updated(p2, score))
+    }
+  }
 }
 
-object Day13 {
+object Day13 extends LazyLogging {
+
+  def parse(line: String): (String, (String, Int)) = {
+
+    //Alice would gain 54 happiness units by sitting next to Bob.
+
+    val tokens = line.split("[\\s.]")
+    val p1 = tokens(0)
+    val gainOrLose = tokens(2)
+    val score = tokens(3).toInt
+    val p2 = tokens(10)
+
+    (p1, (p2, score * (if (gainOrLose == "gain") 1 else -1)))
+
+  }
+
+  def solve(happinessMap: Map[String, Map[String, Int]]): Int = {
+
+    val allSetups: Vector[Vector[String]] =
+      happinessMap.keySet.toVector.permutations.toVector
+
+    allSetups.zipWithIndex.map { case (setup, idx) =>
+      if (idx % 1000 == 0) {
+        logger.debug(s"checking setup #$idx of ${allSetups.length}")
+      }
+      calculateHappinessScore(setup, happinessMap)
+    }.max
+
+  }
+
+  def calculateHappinessScore(seating: Vector[String], happinessMap: Map[String, Map[String, Int]]): Int = {
+    seating.indices.map { i =>
+      val leftNeighborIdx = if (i == 0) seating.size - 1 else i - 1
+      val rightNeighborIdx = if (i == seating.size - 1) 0 else i + 1
+
+      val leftNeighbor = seating(leftNeighborIdx)
+      val me = seating(i)
+      val rightNeighbor = seating(rightNeighborIdx)
+
+      val myDiff = happinessMap(me)(leftNeighbor) + happinessMap(me)(rightNeighbor)
+      myDiff
+    }.sum
+  }
 
 }
--- src/test/scala/day13/Day13Test.scala
+++ src/test/scala/day13/Day13Test.scala
@@ -1,12 +1,40 @@
 package day13
 
+import day13.Day13.solve
 import day13._
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 
 class Day13Part1Test extends AnyFunSuite with Matchers {
 
+  val input = """
+Alice would gain 54 happiness units by sitting next to Bob.
+Alice would lose 79 happiness units by sitting next to Carol.
+Alice would lose 2 happiness units by sitting next to David.
+Bob would gain 83 happiness units by sitting next to Alice.
+Bob would lose 7 happiness units by sitting next to Carol.
+Bob would lose 63 happiness units by sitting next to David.
+Carol would lose 62 happiness units by sitting next to Alice.
+Carol would gain 60 happiness units by sitting next to Bob.
+Carol would gain 55 happiness units by sitting next to David.
+David would gain 46 happiness units by sitting next to Alice.
+David would lose 7 happiness units by sitting next to Bob.
+David would gain 41 happiness units by sitting next to Carol.
+""".trim.split("\n").toList
+
+  test("parser") {
+    Day13.parse("Alice would gain 54 happiness units by sitting next to Bob.") shouldBe ("Alice", ("Bob", 54))
+  }
+
+  test("calculate happiness score of one setup") {
+    val seating = "Alice,Bob,Carol,David".split(",").toVector
+    val happinessMap = part1.calculateHappinessMap(input)
+    Day13.calculateHappinessScore(seating, happinessMap) shouldBe 330
+  }
+
   test("example 1") {
+
+    solve(part1.calculateHappinessMap(input)) shouldBe 330
 
   }
 
@@ -14,8 +42,17 @@ class Day13Part1Test extends AnyFunSuite with Matchers {
 
 class Day13Part2Test extends AnyFunSuite with Matchers {
 
-  test("example 1") {
-
-  }
+  test("example 1") {}
 
 }
+
+/*
+Alice
+Bob
+Carol
+David
+Eric
+Frank
+George
+Mallory
+ */
