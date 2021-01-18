02a7cad598904bbe9affd6e94fd9b7cc5c92b6dd
Florian Witteler <github@flwi.de>
2021-01-18 12:25:56 +0100

    solved day09

--- src/main/scala/day09/day09.scala
+++ src/main/scala/day09/day09.scala
@@ -10,11 +10,23 @@ object part1 {
       .filterNot(_.isEmpty())
       .toList
 
-    val solution = ???
+    val solution = solve(input)
 
     println(
       s"Solution for ${getCallingMainClass.getCanonicalName}: $solution"
     )
+  }
+
+  def solve(input: List[String]) = {
+    // only eight cities
+    // 8! --> 40320 routes
+    // no need for better tsp algorithm - brute force is fast enough
+    val distances = input.map(Day09.parse)
+    val distancesMap = Day09.createDistanceMap(distances)
+    val routes = Day09.calculateRoutes(distancesMap)
+
+    val shortestRoute = routes.minBy(_._2)
+    shortestRoute._2
   }
 }
 
@@ -27,14 +39,75 @@ object part2 {
       .filterNot(_.isEmpty())
       .toList
 
-    val solution = ???
+    val solution = solve(input)
 
     println(
       s"Solution for ${getCallingMainClass.getCanonicalName}: $solution"
     )
   }
+
+  def solve(input: List[String]) = {
+    val distances = input.map(Day09.parse)
+    val distancesMap = Day09.createDistanceMap(distances)
+    val routes = Day09.calculateRoutes(distancesMap)
+
+    val longestRoute = routes.maxBy(_._2)
+    longestRoute._2
+  }
 }
 
 object Day09 {
+
+  def parse(line: String): (String, String, Int) = {
+    //London to Dublin = 464
+    import cats.parse.{Numbers, Parser => P}
+    val word = P.ignoreCaseCharIn('a'.to('z')).rep1(1).string
+
+    val cities = P.rep1Sep(word, 2, P.string(" to "))
+
+    val distanceParser = ((cities <* P.string(" = ")) ~ Numbers.digits).map { case (nel, distance) =>
+      val List(from, to) = nel.toList
+      (from, to, distance.toInt)
+    }
+
+    val result = distanceParser.parseAll(line)
+
+    result match {
+      case Left(value)  => throw new RuntimeException(s"Error during parsing: $value")
+      case Right(value) => value
+    }
+  }
+
+  def createDistanceMap(distances: List[(String, String, Int)]): Map[String, Map[String, Int]] = {
+
+    val distancesBothDirections = distances ++ distances.map { case (from, to, distance) =>
+      (to, from, distance)
+    }
+    val map = distancesBothDirections.map(_._1).distinct.map(from => from -> Map.empty[String, Int]).toMap
+
+    distancesBothDirections.foldLeft(map) { case (acc, (from, to, distance)) =>
+      val updatedFromMap = acc(from).updated(to, distance)
+      acc.updated(from, updatedFromMap)
+    }
+  }
+
+  def calculateRouteLength(distanceMap: Map[String, Map[String, Int]], cities: List[String]): Int = {
+    cities
+      .sliding(2)
+      .map { pair =>
+        val List(from, to) = pair
+        distanceMap(from)(to)
+      }
+      .sum
+  }
+
+  def calculateRoutes(distanceMap: Map[String, Map[String, Int]]): List[(List[String], Int)] = {
+    val permutations = distanceMap.keySet.toList.permutations.toList
+
+    permutations.map { cities =>
+      (cities, calculateRouteLength(distanceMap, cities))
+    }
+
+  }
 
 }
--- src/test/scala/day09/Day09Test.scala
+++ src/test/scala/day09/Day09Test.scala
@@ -1,21 +1,69 @@
 package day09
 
-import day09._
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 
 class Day09Part1Test extends AnyFunSuite with Matchers {
+  val input = """
+London to Dublin = 464
+London to Belfast = 518
+Dublin to Belfast = 141
+""".trim.split("\n").toList
+
+  test("parsing") {
+
+    input.map(Day09.parse) should contain theSameElementsAs List(
+      ("London", "Dublin", 464),
+      ("London", "Belfast", 518),
+      ("Dublin", "Belfast", 141)
+    )
+  }
+
+  test("distances map") {
+    val distances = input.map(Day09.parse)
+    val distancesMap = Day09.createDistanceMap(distances)
+    val actual = distancesMap.flatMap { case (from, toMap) => toMap.map { case (to, distance) => (from, to, distance) } }
+    actual should contain theSameElementsAs List(
+      ("London", "Dublin", 464),
+      ("London", "Belfast", 518),
+      ("Dublin", "Belfast", 141),
+      ("Dublin", "London", 464),
+      ("Belfast", "London", 518),
+      ("Belfast", "Dublin", 141)
+    )
+  }
+
+  test("all routes") {
+    val distances = input.map(Day09.parse)
+    val distancesMap = Day09.createDistanceMap(distances)
+    val routes = Day09.calculateRoutes(distancesMap)
+
+    routes should contain theSameElementsAs List(
+      (List("Dublin", "London", "Belfast"), 982),
+      (List("London", "Dublin", "Belfast"), 605),
+      (List("London", "Belfast", "Dublin"), 659),
+      (List("Dublin", "Belfast", "London"), 659),
+      (List("Belfast", "Dublin", "London"), 605),
+      (List("Belfast", "London", "Dublin"), 982)
+    )
+  }
 
   test("example 1") {
-
+    part1.solve(input) shouldBe 605
   }
 
 }
 
 class Day09Part2Test extends AnyFunSuite with Matchers {
 
-  test("example 1") {
+  val input = """
+London to Dublin = 464
+London to Belfast = 518
+Dublin to Belfast = 141
+""".trim.split("\n").toList
 
+  test("example 1") {
+    part2.solve(input) shouldBe 982
   }
 
 }
