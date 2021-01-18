93627fc69500d3aebd8a4eb8016b2021d79092c4
Florian Witteler <github@flwi.de>
2021-01-18 13:18:42 +0100

    solved day10

--- src/main/scala/day10/day10.scala
+++ src/main/scala/day10/day10.scala
@@ -1,7 +1,12 @@
 package day10
+import com.typesafe.scalalogging.LazyLogging
+import day10.Day10.solve
 import helper.Helper._
 
-object part1 {
+object part1 extends LazyLogging {
+
+  //13:06:28.598 [main] DEBUG day10.Day10$ - playing round #1
+  //13:06:30.602 [main] DEBUG day10.Day10$ - played round #40. Result length: 492982
 
   def main(args: Array[String]) = {
 
@@ -10,15 +15,21 @@ object part1 {
       .filterNot(_.isEmpty())
       .toList
 
-    val solution = ???
+    val solution = solve(input.head, 40)
 
-    println(
+    logger.info(
       s"Solution for ${getCallingMainClass.getCanonicalName}: $solution"
     )
   }
 }
 
-object part2 {
+object part2 extends LazyLogging {
+
+  //13:06:28.598 [main] DEBUG day10.Day10$ - playing round #1
+  //13:06:30.602 [main] DEBUG day10.Day10$ - played round #40. Result length: 492982
+  //13:07:24.612 [main] DEBUG day10.Day10$ - played round #50. Result length: 6989950
+
+  //John Conway showed that you can expand subparts of the game in parallel, if you split it at a group-break.
 
   def main(args: Array[String]) = {
 
@@ -27,14 +38,82 @@ object part2 {
       .filterNot(_.isEmpty())
       .toList
 
-    val solution = ???
+    val solution = solve(input.head, 50)
 
-    println(
+    logger.info(
       s"Solution for ${getCallingMainClass.getCanonicalName}: $solution"
     )
   }
 }
 
-object Day10 {
+object Day10 extends LazyLogging {
+
+  def solve(input: String, numberOfTurns: Int): Int = {
+    val gameOutput = 1
+      .to(numberOfTurns)
+      .foldLeft(input) { case (currentString, roundNo) =>
+        logger.debug(s"playing round #$roundNo")
+        val roundResult = Day10.lookAndSay(currentString)
+        logger.debug(s"played round #$roundNo. Result length: ${roundResult.length}")
+        roundResult
+      }
+
+    gameOutput.length
+  }
+
+  def lookAndSay(str: String): String = {
+
+    val groupBreaks = str
+      .sliding(2)
+      .toList
+      .zipWithIndex
+      .flatMap { case (list, index) =>
+        if (list.distinct.length == 1) List.empty
+        else List(index)
+      }
+
+    /*
+111221133
+
+0    1
+1    1
+2    1  <-
+3    2
+4    2  <-
+5    1
+6    1  <-
+7    3
+8    3  <-
+
+group breaks: 2, 4, 6
+
+groups:
+0-2  length: 3
+3-4  length: 2
+5-6  length: 2
+7-8  length: 2
+     */
+
+    //println("group breaks")
+    //groupBreaks.foreach(println)
+
+    val groups = (-1 :: groupBreaks ::: List(str.length - 1))
+      .sliding(2)
+      .toList
+      .map { case List(from, to) =>
+        (from + 1, to)
+      }
+      .map { case (from, to) =>
+        (from, to, to - from + 1, str(from))
+      }
+
+    //println("groups")
+    //groups.foreach(println)
+
+    groups.map { case (_, _, qty, char) =>
+      s"$qty$char"
+    }.mkString
+
+  }
 
 }
--- src/test/scala/day10/Day10Test.scala
+++ src/test/scala/day10/Day10Test.scala
@@ -1,21 +1,34 @@
 package day10
 
-import day10._
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 
 class Day10Part1Test extends AnyFunSuite with Matchers {
 
   test("example 1") {
+    Day10.lookAndSay("1") shouldBe "11" // one one
+  }
 
+  test("example 2") {
+    Day10.lookAndSay("11") shouldBe "21" //two ones
+  }
+
+  test("example 3") {
+    Day10.lookAndSay("21") shouldBe "1211" //one two and one one
+  }
+
+  test("example 4") {
+    Day10.lookAndSay("1211") shouldBe "111221" //one one, one two, two ones
+  }
+
+  test("example 5") {
+    Day10.lookAndSay("111221") shouldBe "312211" // three 1s, two 2s, and one 1.
   }
 
 }
 
 class Day10Part2Test extends AnyFunSuite with Matchers {
 
-  test("example 1") {
-
-  }
+  test("example 1") {}
 
 }
