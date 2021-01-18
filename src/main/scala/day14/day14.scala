81704d95e96491fbd0dcff1d66cbcb737a1cf689
Florian Witteler <github@flwi.de>
2021-01-18 21:24:36 +0100

    solved day14

--- src/main/scala/day14/day14.scala
+++ src/main/scala/day14/day14.scala
@@ -1,6 +1,8 @@
 package day14
 import helper.Helper._
 
+import scala.concurrent.duration.FiniteDuration
+
 object part1 {
 
   def main(args: Array[String]) = {
@@ -10,12 +12,22 @@ object part1 {
       .filterNot(_.isEmpty())
       .toList
 
-    val solution = ???
+    val solution = solve(input, 2503)
 
     println(
       s"Solution for ${getCallingMainClass.getCanonicalName}: $solution"
     )
   }
+
+  def solve(input: List[String], seconds: Int): Long = {
+    val winner = input
+      .map(Day14.parse)
+      .map(reindeer => (reindeer, Day14.calcDistanceAfter(reindeer, seconds)))
+      .maxBy(_._2)
+
+    winner._2
+  }
+
 }
 
 object part2 {
@@ -27,14 +39,66 @@ object part2 {
       .filterNot(_.isEmpty())
       .toList
 
-    val solution = ???
+    val solution = solve(input, 2503)
 
     println(
       s"Solution for ${getCallingMainClass.getCanonicalName}: $solution"
     )
   }
+
+  def solve(input: List[String], seconds: Int): Long = {
+    val reindeers = input
+      .map(Day14.parse)
+
+    val points = Day14.calcPoints(reindeers, seconds)
+    points.values.max
+  }
 }
 
 object Day14 {
+
+  case class Reindeer(name: String, maxSpeedKmPerS: Int, activeDuration: FiniteDuration, restDuration: FiniteDuration) {}
+
+  def parse(line: String): Reindeer = {
+    import scala.concurrent.duration._
+    //Comet can fly 14 km/s for 10 seconds, but then must rest for 127 seconds.
+    val tokens = line.split(" ")
+    val name = tokens(0)
+    val maxSpeed = tokens(3)
+    val activeDuration = tokens(6)
+    val restDuration = tokens(13)
+
+    Reindeer(name, maxSpeed.toInt, activeDuration.toInt.seconds, restDuration.toInt.seconds)
+  }
+
+  def calcDistanceAfter(reindeer: Reindeer, seconds: Int): Long = {
+    val Reindeer(_, maxSpeedKmPerS, activeDuration, restDuration) = reindeer
+    val wakeSleepCycleLength = activeDuration + restDuration
+
+    val numberOfSleepWakeCycles = seconds / wakeSleepCycleLength.toSeconds
+    val rest = seconds - numberOfSleepWakeCycles * wakeSleepCycleLength.toSeconds
+
+    val restSeconds = if (rest > activeDuration.toSeconds) activeDuration.toSeconds else rest
+    val movingSeconds = numberOfSleepWakeCycles * activeDuration.toSeconds + restSeconds
+    val distance = maxSpeedKmPerS * movingSeconds
+    distance
+  }
+
+  def calcPoints(reindeers: List[Reindeer], seconds: Int): Map[String, Int] = {
+
+    1.to(seconds)
+      .foldLeft(reindeers.map(r => r.name -> 0).toMap) { case (acc, second) =>
+        val distances = reindeers.map(r => (r, calcDistanceAfter(r, second)))
+        val max = distances.map(_._2).max
+
+        val winners = distances.filter(_._2 == max)
+
+        winners.foldLeft(acc) { case (map, (r, _)) =>
+          map.updatedWith(r.name)(_.map(_ + 1))
+        }
+
+      }
+
+  }
 
 }
--- src/test/scala/day14/Day14Test.scala
+++ src/test/scala/day14/Day14Test.scala
@@ -1,21 +1,53 @@
 package day14
 
-import day14._
+import day14.Day14._
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 
+import scala.concurrent.duration.DurationInt
+
 class Day14Part1Test extends AnyFunSuite with Matchers {
 
-  test("example 1") {
+  val input = """
+Comet can fly 14 km/s for 10 seconds, but then must rest for 127 seconds.
+Dancer can fly 16 km/s for 11 seconds, but then must rest for 162 seconds.
+""".trim.split("\n").toList
 
+  test("parse") {
+    input.map(Day14.parse) should contain theSameElementsAs List(
+      Reindeer("Comet", 14, 10.seconds, 127.seconds),
+      Reindeer("Dancer", 16, 11.seconds, 162.seconds)
+    )
+  }
+
+  test("example 1 after 1000s") {
+    val reindeers = input.map(Day14.parse)
+    reindeers.map(calcDistanceAfter(_, 1000)) should contain theSameElementsInOrderAs List(1120, 1056)
+  }
+
+  test("solve") {
+    part1.solve(input, 1000) shouldBe 1120
   }
 
 }
 
 class Day14Part2Test extends AnyFunSuite with Matchers {
 
-  test("example 1") {
+  val input = """
+Comet can fly 14 km/s for 10 seconds, but then must rest for 127 seconds.
+Dancer can fly 16 km/s for 11 seconds, but then must rest for 162 seconds.
+""".trim.split("\n").toList
 
+  test("example 1 after 1000s") {
+    val reindeers = input.map(Day14.parse)
+    calcPoints(reindeers, 1000) shouldBe Map(
+      "Comet" -> 312,
+      "Dancer" -> 689
+    )
+  }
+
+  test("solve") {
+    part2.solve(input, 1000) shouldBe 689
   }
 
 }
