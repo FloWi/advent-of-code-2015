0c3c8e62af2efeb4d8b0d2576e7204233ea46ac2
Florian Witteler <github@flwi.de>
2021-01-23 14:52:11 +0100

    solved day16

--- src/main/scala/day16/day16.scala
+++ src/main/scala/day16/day16.scala
@@ -1,4 +1,5 @@
 package day16
+import Day16._
 import helper.Helper._
 
 object part1 {
@@ -10,12 +11,21 @@ object part1 {
       .filterNot(_.isEmpty())
       .toList
 
-    val solution = ???
+    val giftWrapProps = Day16.parseGiftWrapProps(giftWrapPropsInput.trim.split("\n").toList)
+
+    val isValid: (String, Int, Map[String, Int]) => Boolean = (k, v, map) => map(k) == v
+    val solution = Day16
+      .solve(
+        input,
+        isValid(_, _, giftWrapProps)
+      )
+      .id
 
     println(
       s"Solution for ${getCallingMainClass.getCanonicalName}: $solution"
     )
   }
+
 }
 
 object part2 {
@@ -27,14 +37,80 @@ object part2 {
       .filterNot(_.isEmpty())
       .toList
 
-    val solution = ???
+    val giftWrapProps = Day16.parseGiftWrapProps(giftWrapPropsInput.trim.split("\n").toList)
+
+    val solution = Day16
+      .solve(
+        input,
+        isValid(_, _, giftWrapProps)
+      )
+      .id
 
     println(
       s"Solution for ${getCallingMainClass.getCanonicalName}: $solution"
     )
   }
+
+  def isValid(propertyName: String, value: Int, giftWrapProperties: Map[String, Int]): Boolean = {
+
+    propertyName match {
+      case "cats" | "trees"         => value > giftWrapProperties(propertyName)
+      case "pomerians" | "goldfish" => value < giftWrapProperties(propertyName)
+      case _                        => value == giftWrapProperties(propertyName)
+    }
+  }
+
 }
 
 object Day16 {
+  case class Aunt(id: Int, facts: Map[String, Int])
 
+  val giftWrapPropsInput = """
+children: 3
+cats: 7
+samoyeds: 2
+pomeranians: 3
+akitas: 0
+vizslas: 0
+goldfish: 5
+trees: 3
+cars: 2
+perfumes: 1
+  """
+
+  def parse(line: String): Aunt = {
+
+    // Sue 500: perfumes: 4, cars: 9, trees: 4
+    val (name, factsString) = line.splitAt(line.indexOf(": "))
+
+    val facts = factsString
+      .drop(2)
+      .split(", ")
+      .map { factString =>
+        val List(name, value) = factString.split(": ").toList
+        name -> value.toInt
+      }
+      .toMap
+
+    Aunt(name.replace("Sue ", "").toInt, facts)
+  }
+
+  def parseGiftWrapProps(input: List[String]): Map[String, Int] = {
+    input.map { line =>
+      val List(name, value) = line.split(": ").toList
+      name -> value.toInt
+    }.toMap
+  }
+
+  def solve(input: List[String], isValid: (String, Int) => Boolean): Aunt = {
+
+    val aunts = input
+      .map(Day16.parse)
+
+    aunts
+      .find(aunt => {
+        aunt.facts.forall { case (k, v) => isValid(k, v) }
+      })
+      .get
+  }
 }
--- src/test/scala/day16/Day16Test.scala
+++ src/test/scala/day16/Day16Test.scala
@@ -1,21 +1,24 @@
 package day16
 
-import day16._
+import day16.Day16.Aunt
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 
 class Day16Part1Test extends AnyFunSuite with Matchers {
 
-  test("example 1") {
+  test("parse") {
+    val input = "Sue 500: perfumes: 4, cars: 9, trees: 4"
+
+    Day16.parse(input) shouldBe Aunt(500, Map("perfumes" -> 4, "cars" -> 9, "trees" -> 4))
 
   }
+
+  test("example 1") {}
 
 }
 
 class Day16Part2Test extends AnyFunSuite with Matchers {
 
-  test("example 1") {
-
-  }
+  test("example 1") {}
 
 }
