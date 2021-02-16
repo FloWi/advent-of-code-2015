21cc7378a334aa373a1f93e88f604920514fb57d
Florian Witteler <github@flwi.de>
2021-02-16 23:03:39 +0100

    solved day 17 part 1 and 2
    translated python version found on stackoverflow to scala
    
    https://stackoverflow.com/a/4633515/523722

--- src/main/scala/day17/day17.scala
+++ src/main/scala/day17/day17.scala
@@ -1,4 +1,5 @@
 package day17
+import day17.Day17.getCombinations
 import helper.Helper._
 
 object part1 {
@@ -10,15 +11,29 @@ object part1 {
       .filterNot(_.isEmpty())
       .toList
 
-    val solution = ???
+    val solution = solve(input, eggnogAmount = 150)
 
     println(
       s"Solution for ${getCallingMainClass.getCanonicalName}: $solution"
     )
   }
+
+  def solve(input: List[String], eggnogAmount: Int): Int = {
+
+    val validCombinations = getCombinations(input, eggnogAmount)
+    validCombinations.size
+  }
 }
 
 object part2 {
+
+  /*
+  Find the minimum number of containers that can exactly fit all 150 liters of eggnog.
+  How many different ways can you fill that number of containers and still hold exactly 150 litres?
+
+  In the example above, the minimum number of containers was two.
+  There were three ways to use that many containers, and so the answer there would be 3.
+   */
 
   def main(args: Array[String]) = {
 
@@ -27,14 +42,49 @@ object part2 {
       .filterNot(_.isEmpty())
       .toList
 
-    val solution = ???
+    val solution = solve(input, eggnogAmount = 150)
 
     println(
       s"Solution for ${getCallingMainClass.getCanonicalName}: $solution"
     )
   }
+
+  def solve(input: List[String], eggnogAmount: Int): Int = {
+    val combinations = getCombinations(input, eggnogAmount)
+    val minimumNumberOfContainers = combinations.minBy(_.size).size
+
+    val relevantCombinationsOfThisSize = combinations.filter(_.size == minimumNumberOfContainers)
+    relevantCombinationsOfThisSize.size
+
+  }
+
 }
 
 object Day17 {
+
+  def getCombinations(input: List[String], eggnogAmount: Int): List[List[Int]] = {
+    val containers = input.map(_.toInt).sorted.reverse
+
+    getCombinations(containers, eggnogAmount, Nil)
+  }
+
+  def getCombinations(numbers: List[Int], target: Int, partial: List[Int], results: List[List[Int]] = List.empty): List[List[Int]] = {
+
+    val s = partial.sum
+
+    //check if the partial sum == target
+    if (s == target) {
+      println(s"sum(${partial.mkString(", ")} == $target)")
+      partial :: results
+    } else if (s > target) {
+      results
+    } else {
+      numbers.indices.toList.flatMap { i =>
+        val n = numbers(i)
+        val remaining = numbers.drop(i + 1)
+        getCombinations(remaining, target, n :: partial, results)
+      }
+    }
+  }
 
 }
--- src/test/scala/day17/Day17Test.scala
+++ src/test/scala/day17/Day17Test.scala
@@ -1,13 +1,18 @@
 package day17
 
+import day17.Day17Data.input
 import day17._
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 
+object Day17Data {
+  val input = "20,15,10,5,5".split(",").toList
+}
+
 class Day17Part1Test extends AnyFunSuite with Matchers {
 
   test("example 1") {
-
+    part1.solve(input, 25) shouldBe 4
   }
 
 }
@@ -15,7 +20,6 @@ class Day17Part1Test extends AnyFunSuite with Matchers {
 class Day17Part2Test extends AnyFunSuite with Matchers {
 
   test("example 1") {
-
+    part2.solve(input, 25) shouldBe 3
   }
-
 }
