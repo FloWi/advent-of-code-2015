876bc972738d9fedac7be31d83a7c8eaa9901336
Florian Witteler <github@flwi.de>
2021-02-17 22:11:09 +0100

    solved day18 part 1 and 2

--- src/main/scala/day18/day18.scala
+++ src/main/scala/day18/day18.scala
@@ -10,7 +10,11 @@ object part1 {
       .filterNot(_.isEmpty())
       .toList
 
-    val solution = ???
+    val initial = Day18.parseState(input)
+
+    val finalState = Day18.calcStateAfter(initial, 100)
+
+    val solution = finalState.count(_._2)
 
     println(
       s"Solution for ${getCallingMainClass.getCanonicalName}: $solution"
@@ -27,7 +31,11 @@ object part2 {
       .filterNot(_.isEmpty())
       .toList
 
-    val solution = ???
+    val initial = Day18.parseState(input)
+
+    val finalState = Day18.calcStateAfter(initial, 100, cornerLampsAlwaysOn = true)
+
+    val solution = finalState.count(_._2)
 
     println(
       s"Solution for ${getCallingMainClass.getCanonicalName}: $solution"
@@ -36,5 +44,109 @@ object part2 {
 }
 
 object Day18 {
+
+  type Location = (Int, Int)
+
+  def parseState(lines: List[String]): Map[Location, Boolean] = {
+
+    val state: Map[Location, Boolean] = lines.zipWithIndex.flatMap { case (line, y) =>
+      line.zipWithIndex
+        .map { case (char, x) =>
+          (x, y) -> (char == '#')
+        }
+    }.toMap
+
+    state
+  }
+
+  val neighbors = List(
+    (0, 1), //North
+    (1, 1), //NorthEast
+    (1, 0), //East
+    (1, -1), //SouthEast
+    (0, -1), //South
+    (-1, -1), //SouthWest
+    (-1, 0), //West
+    (-1, 1) //NorthWest
+  )
+
+  def calcNeighbors(state: Map[Location, Boolean], location: Location): List[Location] = {
+
+    neighbors.flatMap { case (dX, dY) =>
+      val (x, y) = location
+      val neighborLocation = (x + dX, y + dY)
+
+      if (state.keySet.contains(neighborLocation)) {
+        List(neighborLocation)
+      } else {
+        Nil
+      }
+    }
+  }
+
+  def calcNumberOfLitNeighbors(state: Map[Location, Boolean], location: Location): Int = {
+    val neighbors = calcNeighbors(state, location)
+
+    neighbors.map(state.apply).count(identity)
+  }
+
+  def calcNewState(state: Map[Location, Boolean]): Map[Location, Boolean] = {
+
+    state.map {
+      case (loc, true) =>
+        //A light which is on stays on when 2 or 3 neighbors are on, and turns off otherwise
+        val cnt = calcNumberOfLitNeighbors(state, loc)
+        loc -> (cnt == 2 || cnt == 3)
+
+      case (loc, false) =>
+        //A light which is off turns on if exactly 3 neighbors are on, and stays off otherwise
+        loc -> (calcNumberOfLitNeighbors(state, loc) == 3)
+    }
+  }
+
+  def turnOnCornerLamps(state: Map[(Int, Int), Boolean]): Map[(Int, Int), Boolean] = {
+    val maxX = state.keys.map(_._1).max
+    val maxY = state.keys.map(_._2).max
+
+    state
+      .updated((0, 0), true)
+      .updated((0, maxY), true)
+      .updated((maxX, 0), true)
+      .updated((maxX, maxY), true)
+  }
+
+  def showState(newState: Map[(Int, Int), Boolean]): List[String] = {
+    val maxX = newState.keys.map(_._1).max
+    val maxY = newState.keys.map(_._2).max
+
+    0.to(maxY)
+      .map { y =>
+        0.to(maxX)
+          .map { x =>
+            val loc = (x, y)
+            val lightState = newState(loc)
+            if (lightState) "#" else "."
+          }
+          .mkString
+      }
+      .toList
+  }
+
+  def calcNewState(lines: List[String]): List[String] = {
+
+    val state = parseState(lines)
+    val newState = calcNewState(state)
+
+    showState(newState)
+  }
+
+  def calcStateAfter(state: Map[(Int, Int), Boolean], n: Int, cornerLampsAlwaysOn: Boolean = false): Map[(Int, Int), Boolean] = {
+
+    1.to(n).foldLeft(state) { case (s, _) =>
+      val updated = calcNewState(s)
+      if (cornerLampsAlwaysOn) turnOnCornerLamps(updated)
+      else updated
+    }
+  }
 
 }
--- src/test/scala/day18/Day18Test.scala
+++ src/test/scala/day18/Day18Test.scala
@@ -4,9 +4,90 @@ import day18._
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 
+object Day18TestData {}
+
 class Day18Part1Test extends AnyFunSuite with Matchers {
 
+  val testInput = s"""
+Initial state:
+.#.#.#
+...##.
+#....#
+..#...
+#.#..#
+####..
+
+After 1 step:
+..##..
+..##.#
+...##.
+......
+#.....
+#.##..
+
+After 2 steps:
+..###.
+......
+..###.
+......
+.#....
+.#....
+
+After 3 steps:
+...#..
+......
+...#..
+..##..
+......
+......
+
+After 4 steps:
+......
+......
+..##..
+..##..
+......
+......
+"""
+  val states = testInput
+    .split("\n\n")
+    .toList
+    .map(_.trim)
+    .map(_.linesIterator.drop(1).toList)
+
+  test("parse state") {
+    val actual = Day18.parseState(states.head)
+
+    actual.count(_._2) shouldBe 3 + 2 + 2 + 1 + 3 + 4
+    actual.size shouldBe 36
+
+    Day18.showState(actual) shouldBe states.head
+  }
+
+  test("neighbors") {
+    Day18.neighbors.distinct.size shouldBe 8
+
+    val state = Day18.parseState(states.head)
+    Day18.calcNeighbors(state, (0, 0)).size shouldBe 3 //top-left-corner
+    Day18.calcNeighbors(state, (1, 0)).size shouldBe 5 //top-row
+    Day18.calcNeighbors(state, (1, 1)).size shouldBe 8 //somewhere in the middle
+  }
+
+  test("calcNumberOfLitNeighbors") {
+    val state = Day18.parseState(states.head)
+    Day18.calcNumberOfLitNeighbors(state, (0, 0)) shouldBe 1 //top-left-corner
+    Day18.calcNumberOfLitNeighbors(state, (2, 0)) shouldBe 3
+  }
+
   test("example 1") {
+
+    val initialState = Day18.parseState(states.head)
+    Day18.calcStateAfter(initialState, 1) shouldBe Day18.parseState(states(1))
+    Day18.calcStateAfter(initialState, 2) shouldBe Day18.parseState(states(2))
+    Day18.calcStateAfter(initialState, 3) shouldBe Day18.parseState(states(3))
+    Day18.calcStateAfter(initialState, 4) shouldBe Day18.parseState(states(4))
+
+    println(states)
 
   }
 
@@ -14,8 +95,67 @@ class Day18Part1Test extends AnyFunSuite with Matchers {
 
 class Day18Part2Test extends AnyFunSuite with Matchers {
 
+  val testInput = s"""
+Initial state:
+##.#.#
+...##.
+#....#
+..#...
+#.#..#
+####.#
+
+After 1 step:
+#.##.#
+####.#
+...##.
+......
+#...#.
+#.####
+
+After 2 steps:
+#..#.#
+#....#
+.#.##.
+...##.
+.#..##
+##.###
+
+After 3 steps:
+#...##
+####.#
+..##.#
+......
+##....
+####.#
+
+After 4 steps:
+#.####
+#....#
+...#..
+.##...
+#.....
+#.#..#
+
+After 5 steps:
+##.###
+.##..#
+.##...
+.##...
+#.#...
+##...#
+"""
+  val states = testInput
+    .split("\n\n")
+    .toList
+    .map(_.trim)
+    .map(_.linesIterator.drop(1).toList)
+
   test("example 1") {
 
+    val initialState = Day18.parseState(states.head)
+    Day18.calcStateAfter(initialState, 1, cornerLampsAlwaysOn = true) shouldBe Day18.parseState(states(1))
+    Day18.calcStateAfter(initialState, 2, cornerLampsAlwaysOn = true) shouldBe Day18.parseState(states(2))
+    Day18.calcStateAfter(initialState, 3, cornerLampsAlwaysOn = true) shouldBe Day18.parseState(states(3))
+    Day18.calcStateAfter(initialState, 4, cornerLampsAlwaysOn = true) shouldBe Day18.parseState(states(4))
   }
-
 }
