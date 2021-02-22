61967a8775e414f9c67ad7c12cf32d72c13d5512
Florian Witteler <github@flwi.de>
2021-02-22 13:27:22 +0100

    solved day 19 part 1 and 2. for part 2 I translated a solution from python i found on reddit

--- src/main/scala/day19/day19.scala
+++ src/main/scala/day19/day19.scala
@@ -1,5 +1,9 @@
 package day19
+import day19.Day19.ParseResult
 import helper.Helper._
+
+import scala.annotation.tailrec
+import scala.util.Random
 
 object part1 {
 
@@ -7,10 +11,12 @@ object part1 {
 
     val input = source(args.headOption)
       .getLines()
-      .filterNot(_.isEmpty())
+      //.filterNot(_.isEmpty())
       .toList
 
-    val solution = ???
+    val instructions = Day19.parse(input.mkString("\n"))
+
+    val solution = Day19.findDistinctMolecules(instructions).size
 
     println(
       s"Solution for ${getCallingMainClass.getCanonicalName}: $solution"
@@ -24,18 +30,119 @@ object part2 {
 
     val input = source(args.headOption)
       .getLines()
-      .filterNot(_.isEmpty())
+      //.filterNot(_.isEmpty())
       .toList
 
-    val solution = ???
+    val instructions = Day19.parse(input.mkString("\n"))
+
+    val solution = findWayFromElectronToStartingMolecule(instructions)
 
     println(
       s"Solution for ${getCallingMainClass.getCanonicalName}: $solution"
     )
   }
+
+  /*
+e => H
+e => O
+H => HO
+H => OH
+O => HH
+HOH
+
+translated python-solution of what-a-baller
+https://www.reddit.com/r/adventofcode/comments/3xflz8/day_19_solutions/cy4cu5b/
+
+replace backwards until you found your destination
+HOH --> HH -> O -> e
+   */
+
+  private def translationFromPythonPart2(parsed: ParseResult): Int = {
+
+    val rules = parsed.replacementRules
+    val rulesBackward: List[(String, String)] = rules.toList.flatMap { case (pattern, list) => list.map(replacement => (replacement, pattern)) }
+
+    var target = parsed.startingMolecule
+    var myRules = Random.shuffle(rulesBackward)
+    var part2 = 0
+    while (target != "e") {
+      val tmp = target
+
+      myRules.foreach { case (b, a) =>
+        if (target.contains(b)) {
+          val old = target
+          val idx = target.indexOf(b)
+          println(s"applied reverse rule $b -> $a at index $idx")
+          target = target.replaceFirst(b, a)
+          println(s"old target: $old")
+          println(s"new target: $target\n")
+          part2 += 1
+        }
+      }
+      println(s"target: $target -- applied all rules.\n")
+
+      if (tmp == target) {
+
+        println(s"target: '$target' - no change found - starting over")
+        target = parsed.startingMolecule
+        part2 = 0
+        myRules = Random.shuffle(myRules)
+      }
+    }
+    part2
+  }
+
+  def findWayFromElectronToStartingMolecule(parsed: ParseResult): Int = {
+
+    val rules = parsed.replacementRules
+    val rulesBackward: List[(String, String)] = rules.toList.flatMap { case (pattern, list) => list.map(replacement => (replacement, pattern)) }
+
+    @tailrec
+    def helper(current: String, rules: List[(String, String)], depth: Int): Int = {
+      if (current == "e") {
+        depth
+      } else {
+        val (moleculeAfterAllRules, replacementCnt) = rules.foldLeft((current, 0)) { case ((str, replacementCnt), (b, a)) =>
+          if (str.contains(b)) {
+            (str.replaceFirst(b, a), replacementCnt + 1)
+          } else (str, replacementCnt)
+        }
+
+        if (moleculeAfterAllRules == current) {
+          //dead-end - start over with different rule-order
+          helper(parsed.startingMolecule, Random.shuffle(rules), 0)
+        } else {
+          helper(moleculeAfterAllRules, rules, depth + replacementCnt)
+        }
+      }
+    }
+
+    helper(parsed.startingMolecule, rulesBackward, 0)
+  }
 }
 
 object Day19 {
+
+  def findDistinctMolecules(replacementRules: Map[String, List[String]], startingMolecule: String): List[String] = {
+    val result = replacementRules.toList.flatMap { case (pattern, replacements) =>
+      val matches = pattern.r.findAllMatchIn(startingMolecule).toList
+      replacements.flatMap { replacement =>
+        matches.map { m =>
+          val before = startingMolecule.substring(0, m.start)
+          val after = startingMolecule.substring(m.`end`)
+          val result = before + replacement + after
+          result
+        }
+      }
+    }
+
+    result.distinct
+  }
+  def findDistinctMolecules(parsed: ParseResult): List[String] = {
+    val ParseResult(replacementRules, startingMolecule) = parsed
+    findDistinctMolecules(replacementRules, startingMolecule)
+
+  }
 
   case class ParseResult(replacementRules: Map[String, List[String]], startingMolecule: String)
 
--- src/test/scala/day19/Day19Test.scala
+++ src/test/scala/day19/Day19Test.scala
@@ -14,7 +14,7 @@ O => HH
 HOH
   """.trim
 
-  test("example 1") {
+  test("example 1 - parsing") {
 
     val parsed = Day19.parse(testInput)
 
@@ -24,13 +24,57 @@ HOH
     )
 
     parsed.startingMolecule shouldBe "HOH"
-
   }
 
+  test("example 1") {
+
+    val parsed = Day19.parse(testInput)
+
+    Day19.findDistinctMolecules(parsed) should contain theSameElementsAs List("HOOH", "HOHO", "OHOH", "HOOH", "HHHH").distinct
+    Day19.findDistinctMolecules(parsed).size shouldBe 4
+  }
+
+  test("example 2") {
+
+    val parsed = Day19.parse(testInput)
+
+    Day19.findDistinctMolecules(parsed.copy(startingMolecule = "HOHOHO")).size shouldBe 7
+
+  }
 }
 
 class Day19Part2Test extends AnyFunSuite with Matchers {
 
-  test("example 1") {}
+  val testInput = """
+e => H
+e => O
+H => HO
+H => OH
+O => HH
 
+HOH
+  """.trim
+
+  test("example 1") {
+
+    val parsed = Day19.parse(testInput)
+
+    parsed.replacementRules shouldBe Map(
+      "e" -> List("H", "O"),
+      "H" -> List("HO", "OH"),
+      "O" -> List("HH")
+    )
+
+    /*
+    starts from e and work your way towards the goal
+
+    e --> H --> OH --> HHH --> dead end - too long
+    e --> O --> HH --> HOH --> success
+     */
+
+    parsed.startingMolecule shouldBe "HOH"
+
+    part2.findWayFromElectronToStartingMolecule(parsed)
+    part2.findWayFromElectronToStartingMolecule(parsed) shouldBe 3
+  }
 }
