82f54cf441d6e5c82417d6e78271531f1c893c5c
Florian Witteler <github@flwi.de>
2021-03-04 11:59:48 +0100

    added day21 parsing

--- src/main/scala/day21/day21.scala
+++ src/main/scala/day21/day21.scala
@@ -3,6 +3,30 @@ import helper.Helper._
 
 object part1 {
 
+  val shopContentInput = """
+Weapons:    Cost  Damage  Armor
+Dagger        8     4       0
+Shortsword   10     5       0
+Warhammer    25     6       0
+Longsword    40     7       0
+Greataxe     74     8       0
+
+Armor:      Cost  Damage  Armor
+Leather      13     0       1
+Chainmail    31     0       2
+Splintmail   53     0       3
+Bandedmail   75     0       4
+Platemail   102     0       5
+
+Rings:      Cost  Damage  Armor
+Damage +1    25     1       0
+Damage +2    50     2       0
+Damage +3   100     3       0
+Defense +1   20     0       1
+Defense +2   40     0       2
+Defense +3   80     0       3
+  """.trim
+
   def main(args: Array[String]) = {
 
     val input = source(args.headOption)
@@ -16,6 +40,7 @@ object part1 {
       s"Solution for ${getCallingMainClass.getCanonicalName}: $solution"
     )
   }
+
 }
 
 object part2 {
@@ -36,5 +61,44 @@ object part2 {
 }
 
 object Day21 {
+  case class ShoppingList(weapons: List[ShoppingItem], armors: List[ShoppingItem], rings: List[ShoppingItem])
+  def parseShoppingList(input: String): ShoppingList = {
+
+    val List(weaponsInput, armorInput, ringsInput) = input.split("\n\n").toList.map(_.linesIterator.toList.tail)
+    //Weapons:    Cost  Damage  Armor
+    //Dagger        8     4       0
+    //Damage +1    25     1       0
+
+    val weapons = weaponsInput.map(ShoppingItem.parse)
+    val armor = armorInput.map(ShoppingItem.parse)
+    val rings = ringsInput.map(ShoppingItem.parse)
+
+    ShoppingList(weapons, armor, rings)
+  }
+
+  def parse(bossInput: String): BossStats = {
+    /*
+      Hit Points: 109
+      Damage: 8
+      Armor: 2
+     */
+
+    val List(hp, damage, armor) = bossInput.linesIterator.toList.map(_.split(": ")(1).toInt)
+    BossStats(hp, damage, armor)
+  }
+
+  case class ShoppingItem(name: String, cost: Int, damage: Int, armor: Int)
+
+  object ShoppingItem {
+    def parse(line: String): ShoppingItem = {
+      val lineItemRegex = "\\s{2,}".r
+
+      val name :: properties = lineItemRegex.split(line).toList
+      val List(cost, damage, armor) = properties.map(_.toInt)
+      ShoppingItem(name, cost, damage, armor)
+    }
+  }
+
+  case class BossStats(hitPoints: Int, damage: Int, armor: Int)
 
 }
--- src/test/scala/day21/Day21Test.scala
+++ src/test/scala/day21/Day21Test.scala
@@ -1,21 +1,57 @@
 package day21
 
-import day21._
+import day21.Day21.ShoppingItem
+import day21.part1.shopContentInput
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers
 
 class Day21Part1Test extends AnyFunSuite with Matchers {
 
-  test("example 1") {
+  val bossInput = """
+    Hit Points: 109
+    Damage: 8
+    Armor: 2
+    """.trim
 
+  test("example 1 - parsing") {
+
+    val actual = Day21.parseShoppingList(shopContentInput)
+    val actualBossStats = Day21.parse(bossInput)
+
+    actual.weapons should contain theSameElementsAs List(
+      ShoppingItem("Dagger", 8, 4, 0),
+      ShoppingItem("Shortsword", 10, 5, 0),
+      ShoppingItem("Warhammer", 25, 6, 0),
+      ShoppingItem("Longsword", 40, 7, 0),
+      ShoppingItem("Greataxe", 74, 8, 0)
+    )
+
+    actual.armors should contain theSameElementsAs List(
+      ShoppingItem("Leather", 13, 0, 1),
+      ShoppingItem("Chainmail", 31, 0, 2),
+      ShoppingItem("Splintmail", 53, 0, 3),
+      ShoppingItem("Bandedmail", 75, 0, 4),
+      ShoppingItem("Platemail", 102, 0, 5)
+    )
+
+    actual.rings should contain theSameElementsAs List(
+      ShoppingItem("Damage +1", 25, 1, 0),
+      ShoppingItem("Damage +2", 50, 2, 0),
+      ShoppingItem("Damage +3", 100, 3, 0),
+      ShoppingItem("Defense +1", 20, 0, 1),
+      ShoppingItem("Defense +2", 40, 0, 2),
+      ShoppingItem("Defense +3", 80, 0, 3)
+    )
+
+    actualBossStats.hitPoints shouldBe 109
+    actualBossStats.damage shouldBe 8
+    actualBossStats.armor shouldBe 2
   }
 
 }
 
 class Day21Part2Test extends AnyFunSuite with Matchers {
 
-  test("example 1") {
-
-  }
+  test("example 1") {}
 
 }
